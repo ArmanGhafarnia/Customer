fd946da79de35756c78401d1946ceef6710169bb
iamarman7 <army.ghafarnia@gmail.com>
2021-06-23 03:40:37 +0430

    foods2

--- lib/inAppPages/restaurant/arman_restaurant_page.dart
+++ lib/inAppPages/restaurant/arman_restaurant_page.dart
@@ -6,6 +6,7 @@ class arman_restaurant_page extends StatefulWidget {
 
 class _arman_restaurant_pageState extends State<arman_restaurant_page> {
   @override
+  bool f = false;
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -17,15 +18,78 @@ class _arman_restaurant_pageState extends State<arman_restaurant_page> {
       body: Row(
         children: [
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 100,vertical: 20),
-            child: Container(
-              height: 250,
-              width: 200,
-              child: Image.network("https://cdn1.iconfinder.com/data/icons/supermarket-cafe-and-stores/50/42-512.png"),
+            padding: const EdgeInsets.symmetric(horizontal: 100,vertical:80),
+            child: Column(
+              children: [
+                Container(
+                  height: 250,
+                  width: 200,
+                  child : Image.network("https://cdn1.iconfinder.com/data/icons/supermarket-cafe-and-stores/50/42-512.png"),
+                ),
+                Center(
+                  child: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        height: 120,
+                        width: 350,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Container(
+                                height: 70,
+                                width: 80,
+                                child: Image.network("https://upload.wikimedia.org/wikipedia/commons/b/b1/Hot_dog_with_mustard.png"),
+                                decoration: BoxDecoration(
+                                ),
+                              ),
+                            ),
+                            Text("Hot dog",style: TextStyle(
+                                fontSize: 20
+                            ),),
+                            Padding(
+                              padding: EdgeInsets.all(11),
+                              child: Container(
+                                height: 30,
+                                width: 70,
+                                child: Switch(
+                                  value: f,
+                                  onChanged: (bool f2){
+                                    setState(() {
+                                      f=f2;
+                                    });
+                                  },
+                                  activeColor: Colors.greenAccent[700],
+                                )
+                              ),
+                            )
+                          ],
+                        ),
+
+                      ),
+                    ),
+                  ),
+                ),
+                    ListView(
+                      children: [
+                        ListTile(
+                          title: Text("hot dog",style: TextStyle(
+                              fontSize: 20
+                          ),),
+                          leading: CircleAvatar(
+                            child: Image.network("https://upload.wikimedia.org/wikipedia/commons/b/b1/Hot_dog_with_mustard.png"),
+                          ),
+                        )
+                      ],
+                    )
+              ],
             ),
-          )
+          ),
         ],
       )
+
     );
   }
 }
